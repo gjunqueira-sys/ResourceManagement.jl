ac4201a64fb60688f5b27ee5fc10882a5b08102d
Documenter.jl <documenter@juliadocs.github.io>
2021-11-24 22:28:19 +0000

    build based on 35c21f4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ResourceManagement","category":"page"},{"location":"#ResourceManagement","page":"Home","title":"ResourceManagement","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ResourceManagement.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ResourceManagement]","category":"page"}]
+}
