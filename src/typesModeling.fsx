1805cb6b8e9e64bdcad3f21b6ddf35aa75d12b5c
gjunqueira-sys <gjunqueira@msn.com>
2022-05-06 12:54:45 -0500

    modeling types

---
+++ src/typesModeling.fsx
@@ -0,0 +1,23 @@
+module LaborTrackerSpreadsheet
+    // module declaration
+
+type LaborTracker = {
+
+    Department: string      //ie 430300 ENG - Controls
+    Project: string         // ie 142270
+    Description: string
+    Network: string         // ie N150547-02
+    Task: int               // ie 2050
+    Employee_No:int
+    Employee_Name: string
+    MM_YYYY_Actual:string // sequence repeated 24 times
+    MM_YYYY_Planned:string // sequence repeated 24 times
+    MM_YYYY_Difference:string   // sequence repeated 24 times
+
+}
+
+
+
+
+
+
